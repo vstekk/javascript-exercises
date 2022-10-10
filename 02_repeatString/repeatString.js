86786d9660694e46bcf900444f2b9c83ea0fbb35
vstekk <jakub.racek@gmail.com>
2022-10-10 21:37:55 +0200

    first two excersises completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let result = "";
+    if (number < 0) {
+        return 'ERROR';
+    }
+    for (i = 0; i < number; i++){
+        result = result + string;
+    }
+    return result;
 };
 
 // Do not edit below this line
