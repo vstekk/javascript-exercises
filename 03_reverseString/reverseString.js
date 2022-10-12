6dd5ae2c118567c3ece640132a73ee1e5af37c62
vstekk <jakub.racek@gmail.com>
2022-10-12 18:41:19 +0200

    three and four done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let inputArray = Array.from(input);
+    const result = [];
+    for (i = 0; i = inputArray.length; i++){
+        result.push(inputArray.pop());
+    }
+    return result.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    const inputArray = arguments[0];
+    for (i = 1; i < arguments.length; i++){
+        for (j = 0; j < inputArray.length; j++){
+            if (inputArray[j] === arguments[i]) {
+                inputArray.splice(j, 1);
+            }
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
