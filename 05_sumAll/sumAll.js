47ab73e761aaa394b193c08511eebe6d9ac28273
vstekk <jakub.racek@gmail.com>
2022-10-12 21:40:42 +0200

    5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
 const sumAll = function() {
+    const arr = [...arguments];
+    const hasError = arr.some(x => typeof x !== "number" || x < 0);
+    
+    if (hasError) {
+        return 'ERROR';
+    }
 
+    const lowest = Math.min(...arr);
+    const highest = Math.max(...arr);
+
+    let current = lowest;
+    let result = 0;
+    do {
+        result = result + current++;
+    } while (current <= highest);
+    return result;
 };
 
 // Do not edit below this line
