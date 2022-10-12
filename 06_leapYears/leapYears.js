bf6f2425d048608e377aef2d8ce5080ff96d8176
vstekk <jakub.racek@gmail.com>
2022-10-12 21:58:16 +0200

    6 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,7 @@
-const leapYears = function() {
-
-};
+const leapYears = (year) =>
+    year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)
+    ? true
+    : false;
 
 // Do not edit below this line
 module.exports = leapYears;
