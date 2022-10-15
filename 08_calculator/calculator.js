073a37fa5769280500f77baa730a1f660176587e
vstekk <jakub.racek@gmail.com>
2022-10-15 17:14:58 +0200

    7 8 9 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  const initialValue = 0;
+  return arr.reduce((previousValue, currentValue) => 
+  previousValue + currentValue, initialValue);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((previousValue, currentValue) =>
+    previousValue * currentValue);
 };
 
-const power = function() {
-	
+const power = function(number, times) {
+  let current = number;
+	for (i = 1; i < times; i++){
+    current = current * number;
+  }
+  return current;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  var current = 1;
+  for (var i = 2; i <= number; i++)
+    current = current * i;
+  return current;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+    const clean = [...palindrome.toLowerCase()]
+    .filter(s => s.charCodeAt(0) >= 97 && s.charCodeAt(0) <= 122);
 
+    const reverse = [...clean].reverse();
+    return clean.every((char, index) => char === reverse[index]);
 };
 
 // Do not edit below this line
