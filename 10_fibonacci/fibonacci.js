dfe5d432b16ad1b94a069092fabc5bd3e8f4da60
vstekk <jakub.racek@gmail.com>
2022-10-17 20:04:45 +0200

    9 and 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(length) {
+    let first = 1;
+    let second = 1;
+    let next = 1;
 
+    const n = parseInt(length);
+    if (n < 1 || isNaN(n)) {
+        return 'OOPS';
+    }
+
+    for (i = 2; i < n; i++){
+        next = first + second;
+        first = second;
+        second = next;
+    }
+    return next;
 };
 
 // Do not edit below this line
